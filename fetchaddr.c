3e852775596bc941c37c07560329740fb0755763
Roland Rosenfeld <roland@debian.org>
1998-10-14 14:47:58 +0000

    Initial revision

---
+++ fetchaddr.c
@@ -0,0 +1,150 @@
+/*
+ *  Copyright (C) 1998  Thomas Roessler <roessler@guug.de>
+ *
+ *  This program is free software; you can redistribute
+ *  it and/or modify it under the terms of the GNU
+ *  General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License,
+ *  or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE.  See the GNU General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU General
+ *  Public License along with this program; if not, write
+ *  to the Free Software Foundation, Inc., 675 Mass Ave,
+ *  Cambridge, MA 02139, USA.
+ *
+ */
+
+/* $Id$ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "helpers.h"
+#include "rfc822.h"
+#include "rfc2047.h"
+
+struct header
+{
+  const char *tag;
+  char *value;
+  size_t len;
+  size_t taglen;
+};
+
+struct header hdr[] =
+{
+  { "to:",		NULL, 0,  3 },
+  { "from:",		NULL, 0,  5 },
+  { "cc:",		NULL, 0,  3 },
+  { "resent-from:",	NULL, 0, 12 },
+  { "resent-to:",	NULL, 0, 10 },
+  { NULL, 		NULL, 0,  0 }
+};
+
+void chop(struct header *cur)
+{
+  if(cur->len && cur->value[cur->len - 1] == '\n')
+    cur->value[--cur->len] = '\0';
+}
+
+int writeout(struct header *h)
+{
+  int rv = 0, i;
+  ADDRESS *addr, *p;
+  time_t timep;
+
+  if(!h->value)
+    return;
+  
+  addr = rfc822_parse_adrlist(NULL, h->value);
+  time(&timep);
+
+  rfc2047_decode_adrlist(addr);
+  for(p = addr; p; p = p->next)
+  {
+    if(!p->group && p->mailbox && *p->mailbox && p->personal && *p->personal)
+    {
+      if(p->personal && strlen(p->personal) > 30)
+	strcpy(p->personal + 27, "...");
+      
+      printf("%s\t%s\t%s", p->mailbox, p->personal && *p->personal ? 
+	     p->personal : "<unknown>",
+	     ctime(&timep));
+
+      rv = 1;
+    }
+  }
+
+  rfc822_free_address(&addr);
+
+  return rv;
+}
+  
+main()
+{
+  char buff[2048];
+  char *t;
+  int i, rv;
+  int partial = 0;
+  struct header *cur_hdr;
+
+  while(fgets(buff, sizeof(buff), stdin))
+  {
+    
+    if(!partial && *buff == '\n')
+      break;
+
+    if(cur_hdr && (partial || *buff == ' ' || *buff == '\t'))
+    {
+      size_t nl = cur_hdr->len + strlen(buff);
+      
+      safe_realloc((void **) &cur_hdr->value, nl + 1);
+      strcpy(cur_hdr->value + cur_hdr->len, buff);
+      cur_hdr->len = nl;
+      chop(cur_hdr);
+    }
+    else if(!partial && *buff != ' ' && *buff != '\t')
+    {
+      cur_hdr = NULL;
+
+      for(i = 0; hdr[i].tag; i++)
+      {
+	if(!strncasecmp(buff, hdr[i].tag, hdr[i].taglen))
+	{
+	  cur_hdr = &hdr[i];
+	  break;
+	}
+      }
+      
+      if(cur_hdr)
+      {
+	safe_free((void **) &cur_hdr->value);
+	cur_hdr->value = safe_strdup(buff + cur_hdr->taglen);
+	cur_hdr->len = strlen(cur_hdr->value);
+	chop(cur_hdr);
+      }
+    }
+    
+    if(!(t = strchr(buff, '\n')))
+      partial = 1;
+    else
+      partial = 0;
+  }
+
+  for(rv = 0, i = 0; hdr[i].tag; i++)
+  {
+    if(hdr[i].value)
+      rv = writeout(&hdr[i]) || rv;
+  }
+
+  return (rv ? 0 : 1);
+  
+}
---
+++ rfc2047.c
@@ -0,0 +1,223 @@
+/*
+ * Copyright (C) 1996-8 Michael R. Elkins <me@cs.hmc.edu>
+ * 
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */ 
+
+/* $Id$ */
+
+#include <ctype.h>
+#include <string.h>
+
+#include "rfc822.h"
+#include "rfc2047.h"
+#include "helpers.h"
+
+enum
+{
+  ENCOTHER,
+  ENC7BIT,
+  ENC8BIT,
+  ENCQUOTEDPRINTABLE,
+  ENCBASE64,
+  ENCBINARY
+};
+
+const char MimeSpecials[] = "@.,;<>[]\\\"()?/=";
+const char Charset[] = "iso-8859-1"; /* XXX - hack */
+
+
+int Index_hex[128] = {
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+     0, 1, 2, 3,  4, 5, 6, 7,  8, 9,-1,-1, -1,-1,-1,-1,
+    -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1
+};
+
+int Index_64[128] = {
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+    -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,62, -1,-1,-1,63,
+    52,53,54,55, 56,57,58,59, 60,61,-1,-1, -1,-1,-1,-1,
+    -1, 0, 1, 2,  3, 4, 5, 6,  7, 8, 9,10, 11,12,13,14,
+    15,16,17,18, 19,20,21,22, 23,24,25,-1, -1,-1,-1,-1,
+    -1,26,27,28, 29,30,31,32, 33,34,35,36, 37,38,39,40,
+    41,42,43,44, 45,46,47,48, 49,50,51,-1, -1,-1,-1,-1
+};
+
+
+#define IsPrint(c) (isprint((unsigned char)(c)) || \
+	((unsigned char)(c) >= 0xa0))
+
+#define hexval(c) Index_hex[(unsigned int)(c)]
+#define base64val(c) Index_64[(unsigned int)(c)]
+
+static int rfc2047_decode_word (char *d, const char *s, size_t len)
+{
+  char *p = safe_strdup (s);
+  char *pp = p;
+  char *pd = d;
+  int enc = 0, filter = 0, count = 0, c1, c2, c3, c4;
+
+  while ((pp = strtok (pp, "?")) != NULL)
+  {
+    count++;
+    switch (count)
+    {
+      case 2:
+	if (strcasecmp (pp, Charset) != 0)
+	  filter = 1;
+	break;
+      case 3:
+	if (toupper (*pp) == 'Q')
+	  enc = ENCQUOTEDPRINTABLE;
+	else if (toupper (*pp) == 'B')
+	  enc = ENCBASE64;
+	else
+	  return (-1);
+	break;
+      case 4:
+	if (enc == ENCQUOTEDPRINTABLE)
+	{
+	  while (*pp && len > 0)
+	  {
+	    if (*pp == '_')
+	    {
+	      *pd++ = ' ';
+	      len--;
+	    }
+	    else if (*pp == '=')
+	    {
+	      *pd++ = (hexval(pp[1]) << 4) | hexval(pp[2]);
+	      len--;
+	      pp += 2;
+	    }
+	    else
+	    {
+	      *pd++ = *pp;
+	      len--;
+	    }
+	    pp++;
+	  }
+	  *pd = 0;
+	}
+	else if (enc == ENCBASE64)
+	{
+	  while (*pp && len > 0)
+	  {
+	    c1 = base64val(pp[0]);
+	    c2 = base64val(pp[1]);
+	    *pd++ = (c1 << 2) | ((c2 >> 4) & 0x3);
+	    if (--len == 0) break;
+	    
+	    if (pp[2] == '=') break;
+
+	    c3 = base64val(pp[2]);
+	    *pd++ = ((c2 & 0xf) << 4) | ((c3 >> 2) & 0xf);
+	    if (--len == 0)
+	      break;
+
+	    if (pp[3] == '=')
+	      break;
+
+	    c4 = base64val(pp[3]);
+	    *pd++ = ((c3 & 0x3) << 6) | c4;
+	    if (--len == 0)
+	      break;
+
+	    pp += 4;
+	  }
+	  *pd = 0;
+	}
+	break;
+    }
+    pp = 0;
+  }
+  safe_free ((void **) &p);
+  if (filter)
+  {
+    pd = d;
+    while (*pd)
+    {
+      if (!IsPrint (*pd))
+	*pd = '?';
+      pd++;
+    }
+  }
+  return (0);
+}
+
+/* try to decode anything that looks like a valid RFC2047 encoded
+ * header field, ignoring RFC822 parsing rules
+ */
+void rfc2047_decode (char *d, const char *s, size_t dlen)
+{
+  const char *p, *q;
+  size_t n;
+  int found_encoded = 0;
+
+  dlen--; /* save room for the terminal nul */
+
+  while (*s && dlen > 0)
+  {
+    if ((p = strstr (s, "=?")) == NULL ||
+	(q = strchr (p + 2, '?')) == NULL ||
+	(q = strchr (q + 1, '?')) == NULL ||
+	(q = strstr (q + 1, "?=")) == NULL)
+    {
+      /* no encoded words */
+      if (d != s)
+	strfcpy (d, s, dlen + 1);
+      return;
+    }
+
+    if (p != s)
+    {
+      n = (size_t) (p - s);
+      /* ignore spaces between encoded words */
+      if (!found_encoded || strspn (s, " \t\r\n") != n)
+      {
+	if (n > dlen)
+	  n = dlen;
+	if (d != s)
+	  memcpy (d, s, n);
+	d += n;
+	dlen -= n;
+      }
+    }
+
+    rfc2047_decode_word (d, p, dlen);
+    found_encoded = 1;
+    s = q + 2;
+    n = strlen (d);
+    dlen -= n;
+    d += n;
+  }
+  *d = 0;
+}
+
+void rfc2047_decode_adrlist (ADDRESS *a)
+{
+  while (a)
+  {
+    if (a->personal && strstr (a->personal, "=?") != NULL)
+      rfc2047_decode (a->personal, a->personal, strlen (a->personal) + 1);
+    a = a->next;
+  }
+}
