1cead494d7022f4265c4c7721b56c0026cc7ab0d
Roland Rosenfeld <roland@debian.org>
2000-08-05 19:22:46 +0000

    * Add new helper program qpto8bit, which converts quoted-printable     (according to RFC2047) to plain 8bit (without MIME header).  This is     used by m_pine to decode quoted-printable in addressbooks to 8bit.

---
+++ qpto8bit.c
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (C) 2000  Roland Rosenfeld <roland@spinnaker.de>
+ *
+ *  This program is free software; you can redistribute
+ *  it and/or modify it under the terms of the GNU
+ *  General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License,
+ *  or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will
+ *  be useful, but WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE.  See the GNU General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU General
+ *  Public License along with this program; if not, write
+ *  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA  02111, USA.
+ *
+ */
+
+/* $Id$ */
+
+#include <stdio.h>
+
+#include "helpers.h"
+#include "rfc822.h"
+#include "rfc2047.h"
+
+int main ()
+{
+  char buff[2048];
+
+  while (fgets (buff, sizeof (buff), stdin)) {
+    rfc2047_decode (buff, buff, sizeof (buff));
+    fputs (buff, stdout);
+  }
+}
