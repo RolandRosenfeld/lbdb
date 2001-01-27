629035b697a30a013b0c7500f1faa58fbbfacd39
Roland Rosenfeld <roland@debian.org>
2001-01-27 16:07:46 +0000

    * Add directory for Richard Hawes' changes.  I still don't like his     changes, so I don't merge it into the binary package, but conserve     them in the source package, for later use (Closes: #31018).

---
+++ lbdb_bbdb_query.el
@@ -0,0 +1,28 @@
+;;; query the bbdb for all records matching a given string; return them
+;;; in lbdb-suitable format 
+;;; 
+;;; This code is (c) Utz-Uwe Haus <haus@uuhaus.de> 2001
+;;; Use, reuse and deletion are permitted in any way you can up with.
+
+(require 'bbdb)
+(require 'bbdb-com)
+
+(defun lbdb-bbdb-query (string)
+  "Display all entries in the BBDB matching STRING in the Name, Company or
+email fields in lbdb-usable format, i.e. 
+<email-address>[TAB]<Full Name>[TAB]<comment>
+where <comment> is BBDB:timestamp."
+  (let ((matches
+	 (bbdb-search (bbdb-records) string string string nil nil)))
+    (mapcar 
+     (lambda (record) 
+       (let ((name (bbdb-record-name record))
+	     (timestamp (bbdb-record-getprop record 'timestamp)))
+	 (mapcar
+	  (lambda (this-email)
+	    (princ (format "%s\t%s\tBBDB:%s\n" this-email name timestamp)))
+	  (bbdb-record-net record))))
+     matches)))
+
+; use like:
+;(lbdb-bbdb-query "grae")
