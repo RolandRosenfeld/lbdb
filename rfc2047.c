9c95103004d4428272018681535df4048af83c9d
Roland Rosenfeld <roland@debian.org>
2022-02-27 23:06:38 +0100

    rfc2047: Update copyright and fix some warnings.

--- rfc2047.c
+++ rfc2047.c
@@ -1,20 +1,21 @@
 /*
- * Copyright (C) 1996-8 Michael R. Elkins <me@cs.hmc.edu>
- * 
+ * Copyright (C) 1996-2000,2010 Michael R. Elkins <me@mutt.org>
+ * Copyright (C) 2000-2002 Edmund Grimley Evans <edmundo@rano.org>
+ *
  *     This program is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation; either version 2 of the License, or
  *     (at your option) any later version.
- * 
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- * 
+ *
  *     You should have received a copy of the GNU General Public License
- *     along with this program; if not, write to the Free Software Foundation,
- *     Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,, USA.
- */ 
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 #include <ctype.h>
 #include <string.h>
@@ -139,7 +140,7 @@ static int rfc2047_decode_word (char *d, const char *s, size_t dlen)
 	    c2 = base64val(pp[1]);
 	    *pd++ = (c1 << 2) | ((c2 >> 4) & 0x3);
 	    if (--len == 0) break;
-	    
+
 	    if (pp[2] == '=') break;
 
 	    c3 = base64val(pp[2]);
--- rfc2047.h
+++ rfc2047.h
@@ -1,25 +1,25 @@
 /*
- * Copyright (C) 1996-8 Michael R. Elkins <me@cs.hmc.edu>
- * 
+ * Copyright (C) 1996-2000 Michael R. Elkins <me@mutt.org>
+ *
  *     This program is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation; either version 2 of the License, or
  *     (at your option) any later version.
- * 
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- * 
+ *
  *     You should have received a copy of the GNU General Public License
- *     along with this program; if not, write to the Free Software Foundation,
- *     Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301,, USA.
- */ 
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <sys/types.h>
+#include "rfc822.h"
 
 extern const char *Charset;
-void rfc2047_encode_string (char *, size_t, const unsigned char *);
-void rfc2047_encode_adrlist (ADDRESS *);
 
 void rfc2047_decode (char *, const char *, size_t);
 void rfc2047_decode_adrlist (ADDRESS *);
-
